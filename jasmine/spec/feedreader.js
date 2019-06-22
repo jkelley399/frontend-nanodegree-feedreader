8e9c59977b973f0602c8e72c1a59480c52fda70d
jkelley399 <jkelley@399innovation.com>
2019-06-21 20:57:46 -0700

    feat wrote two tests for the 'RSS Feeds' test suite
    'and all their URLs are defined and non-empty'
    'and all their names are defined and non-empty'
    
    Also outlined the 'The menu' test suite
    
    TODO: Further tests showed Gulp env not working

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,37 +26,80 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
+        /* the for...loop in each of the following two tests is based upon
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript
+         * /Reference/Statements/for...of
+         * consulted 2019-06-21
+         */
+        it('and all their URLs are defined and non-empty', function() {
+            for (let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect((feed.url).length).not.toBe(0);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('and all their names are defined and non-empty', function() {
+            for (let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect((feed.name).length).not.toBe(0);
+            }
+        });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
-
+    /* Second test suite, which is about the behavior of the menu.*/
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+     // NOTE 2019-06-21: This behavior seems to be controlled by index.html
+    // <body class="menu-hidden">
+    //     <div class="header">
+    //         <a href="#" class="menu-icon-link">
+    //             <i class="icon-list"></i>
+    //         </a>
 
-         /* TODO: Write a test that ensures the menu changes
+    //         <h1 class="header-title">Feeds</h1>
+    //     </div>
+
+        it('is hidden by default', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+     // NOTE 2019-06-21: This behavior seems to be controlled by app.js:
 
+    /* When the menu icon is clicked on, we need to toggle a class
+     * on the body to perform the hiding/showing of our menu.
+     */
+     //    menuIcon.on('click', function() {
+     //        $('body').toggleClass('menu-hidden');
+     //    });
+
+        it('changes visibly when menu icon clicked', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+    /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -65,8 +108,14 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
 }());
+
+
+// Materials relied upon in addition to Udacity course material
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+
+// consulted 2019-06-21
